110813321ba55ee8b7f4576d08d6c53d7413b441
MintTeaNeko <43279771+MintTeaNeko@users.noreply.github.com>
2022-07-27 04:07:01 +0300

    Add files via upload

---
+++ src/AssetMod/Mod.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using ModLoader;
+using UnityEngine;
+
+namespace AssetMod
+{
+    public class Mod : MonoBehaviour
+    {
+        private string modPath;
+        private ModInformation information;
+        private ModSettings settings;
+        private EventManager manager;
+        private Character character;
+        
+        private void Start()
+        {
+            manager = FindObjectOfType<EventManager>(true);
+            manager.NotifyPlayer("assetmod has loaded", 0);
+            modPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            information = JsonUtility.FromJson<ModInformation>(File.ReadAllText(Path.Combine(modPath, "mod.json")));
+            settings = JsonUtility.FromJson<ModSettings>(File.ReadAllText(Path.Combine(modPath, "mod_settings.json")));
+            character = manager.Student[0];
+            
+            Catalog.LoadObjectAsset(Path.Combine(modPath, "crownbundle"), information.objAddresses[0], obj =>
+            {
+                obj.transform.position = character.MyHair.gameObject.transform.position + new Vector3(settings.offSetX, settings.offSetY, settings.offSetZ);
+                obj.transform.parent = character.MyHair.gameObject.transform;
+                obj.transform.localScale = new Vector3(1, 1, 1);
+                Material mat = new Material(Shader.Find("Standard"));
+                mat.color = new Color(settings.ColorR/255, settings.ColorG/255, settings.ColorB/255);
+                
+                obj.GetComponent<MeshRenderer>().material = mat;
+            });
+        }
+    }
+
+    public class ModSettings
+    {
+        public float offSetX;
+        public float offSetY;
+        public float offSetZ;
+        public float ColorR;
+        public float ColorG;
+        public float ColorB;
+    }
+}
---
+++ src/FirstMod/firstmod.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using ModLoader;
+using UnityEngine;
+
+namespace firstmod
+{
+    public class firstmod : MonoBehaviour
+    {
+        private EventManager eventManager;
+        private Character character;
+        private bool isDebounce = false;
+
+        private void Start()
+        {
+            eventManager = FindObjectOfType<EventManager>(true);
+            eventManager.NotifyPlayer("first mod ever has loaded!", 0);
+            character = eventManager.Student[0];
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                if (!isDebounce)
+                {
+                    eventManager.NotifyPlayer("nice new oppais <3", 0);
+                    int random = UnityEngine.Random.Range(0, 6);
+                    for (int i = 0; i < 2; i++)
+                    {
+                        character.OppaiGO[i].GetComponent<Oppai>().ChangeCupSize(random);
+                    }
+                }
+
+                isDebounce = true;
+            }
+            else
+            {
+                isDebounce = false;
+            }
+        }
+    }
+}
