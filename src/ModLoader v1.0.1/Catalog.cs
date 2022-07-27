b3b472e23ef103bd637f52140a08870a0697893a
MintTeaNeko <43279771+MintTeaNeko@users.noreply.github.com>
2022-07-27 18:37:07 +0300

    Add files via upload

---
+++ src/ModLoader v1.0.1/Catalog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace ModLoader
+{
+    public class Catalog : MonoBehaviour
+    {
+        public static void LoadObjectAsset(string path, string address, Action<GameObject> callbackMethod)
+        {
+            try
+            {
+                var assetBundle = AssetBundle.LoadFromFile(path);
+                var prefab = assetBundle.LoadAsset<GameObject>(address);
+                callbackMethod(Instantiate(prefab));
+                assetBundle.Unload(false);
+            }
+            catch (Exception e)
+            {
+                ModManager.LogToFile($"error loading asset {address}: {e}");
+            }
+        }
+    }
+}
---
+++ src/ModLoader v1.0.1/Loader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using System.IO;
+
+namespace ModLoader
+{
+    public class Loader : MonoBehaviour
+    {
+        private void Start()
+        {
+            ModManager.LogToFile("Loader is ready!");
+            string[] directories = Directory.GetDirectories(ModManager.modsFolder);
+            for (int i = 0; i < directories.Length; i++)
+            {
+                string directory = directories[i];
+                string[] files = Directory.GetFiles(directory);
+                for (int n = 0; n < files.Length; n++)
+                {
+                    string file = files[n];
+                    if (file.Contains("mod.json"))
+                    {
+                        try
+                        {
+                            ModInformation modInformation = JsonUtility.FromJson<ModInformation>(File.ReadAllText(file));
+                            string[] splitType = modInformation.type.Split(',');
+                            string assemblyName = splitType[1].Replace(" ", "") + ".dll";
+                            Type mono = Assembly.LoadFile(Path.Combine(directory, assemblyName)).GetType(splitType[0]);
+                            GameObject go = new GameObject();
+                            go.AddComponent(mono);
+                        }
+                        catch (Exception e)
+                        {
+                            ModManager.LogToFile($"failed to load {file}: {e}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class ModInformation
+    {
+        public string type;
+        public string modName;
+        public string modGameVersion;
+        public List<string> objAddresses;
+    }
+}
